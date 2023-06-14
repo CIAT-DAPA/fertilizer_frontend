5665c89949bf5ae75dbc4c7692a528daf6136821
Victor Hernandez <victor.993_@hotmail.com>
2023-06-14 10:19:23 -0500

    FETH-125 Add dominant layer to the options in the website

---
+++ src/src/components/select_filters/SelectFilters.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Select from 'react-select';
+import infoIcon from '../../assets/icons/info-icon.svg';
+
+import './SelectFilters.css';
+
+function SelectFilters(props) {
+
+    return (
+        <div className='d-flex justify-content-start'>
+            <div className='d-flex justify-content-end font-link-body flex-wrap align-items-center'>
+                <h5 className='p-2 bd-highlight mt-2'>Crop</h5>
+                <div className='p-2 bd-highlight select'>
+                    <Select 
+                        defaultValue={props.opt_crops[0]}
+                        options={props.opt_crops}
+                        onChange={props.onChangeCrop}
+                        menuPortalTarget={document.body} 
+                        styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+                    />
+                </div>
+            </div>
+
+            <div className='d-flex justify-content-end font-link-body flex-wrap align-items-center'>
+                <h5 className='p-2 bd-highlight mt-2'>Forecast date</h5>
+                <div className='p-2 bd-highlight select'>
+                    <Select defaultValue={props.opt_forecast[0]}
+                        options={props.opt_forecast}
+                        onChange={props.onChangeForecast}
+                        menuPortalTarget={document.body} 
+                        styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SelectFilters;
