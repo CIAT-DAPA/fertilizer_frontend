d53ced8e3f07020048d8a53c1fe43dedd5f4ff0b
Daniel Esteban Guzmán <estebantoon@hotmail.com>
2023-11-27 13:32:50 -0500

    Update App.test.js

--- src/src/App.test.js
+++ src/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/NPS & Urea/i);
+  const linkElement = screen.getByText(/Select a country/i);
   expect(linkElement).toBeInTheDocument();
 });
